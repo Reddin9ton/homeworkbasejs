d49768b4c53ca7e9a1f356c63192a649d549664d
red <artikus256@gmail.com>
2023-12-03 19:01:10 +0300

    Первое задание полностью готово, второе нужна помощь

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = (b**2) - 4 * a * c;
+  let rootOne = 0;
+  let rootTwo = 0;
+
+  if (discr < 0) {
+    arr = [];
+  }
+  if (discr === 0) {
+    rootOne = -b / (2 * a);
+    arr = [rootOne];
+
+  }
+
+  if (discr > 0) {
+    rootOne = (-b + Math.sqrt(discr) )/(2*a);
+    rootTwo = (-b - Math.sqrt(discr) )/(2*a);
+    arr = [rootOne, rootTwo];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = (percent / 100) / countMonths;
+  let S = amount - contribution;
+  let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let sum = Number((payment * countMonths).toFixed (2));
+  return sum;
+
 }
