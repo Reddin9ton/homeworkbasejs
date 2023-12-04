31f90e06442db16ea10ff8ad29f78f778aaf7221
red <artikus256@gmail.com>
2023-12-04 19:25:09 +0300

    внесены правки по дз

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,32 +2,26 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discr = (b**2) - 4 * a * c;
-  let rootOne = 0;
-  let rootTwo = 0;
 
-  if (discr < 0) {
-    arr = [];
-  }
   if (discr === 0) {
-    rootOne = -b / (2 * a);
-    arr = [rootOne];
+   let rootOneForm = -b / (2 * a);
+    arr = [rootOneForm];
 
   }
 
   if (discr > 0) {
-    rootOne = (-b + Math.sqrt(discr) )/(2*a);
-    rootTwo = (-b - Math.sqrt(discr) )/(2*a);
-    arr = [rootOne, rootTwo];
+   let rootOneForm = (-b + Math.sqrt(discr) )/(2*a);
+   let rootTwoForm = (-b - Math.sqrt(discr) )/(2*a);
+    arr = [rootOneForm, rootTwoForm];
   }
 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = (percent / 100) / countMonths;
+  let P = (percent / 100) / 12;
   let S = amount - contribution;
   let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
-  let sum = Number((payment * countMonths).toFixed (2));
-  return sum;
+  return Number((payment * countMonths).toFixed (2));;
 
 }
